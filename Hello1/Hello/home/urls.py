72c799fcdea39cf52616ef28df1b0b9cb059f66f
adnan-119 <105105995+adnan-119@users.noreply.github.com>
2023-11-05 02:26:01 +0530

    Initial commit

---
+++ Hello1/Hello/home/urls.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.urls import path,include 
+from home import views
+
+
+urlpatterns = [
+
+    path("register",views.registerPage,name='regsiter'),
+
+    path("login",views.loginPage,name='login'),
+    
+    path("logout",views.logoutUser,name='logout'), 
+    
+    path("",views.index,name='home'),
+    
+    path("about",views.about,name='about'),
+    
+    path("services",views.services,name='services'),
+
+    path("driverbookingcar",views.driverbookingcar,name='driverbookingcar'),
+
+    path("carservices",views.carservices,name='carservices'),
+
+    path("mechanicservices",views.mechanicservices,name='mechanicservices'),
+
+    path("carbooking",views.carbooking,name='carbooking'),
+    
+    path("confirm",views.confirm,name='confirm'),
+
+    path("payment",views.payment,name='payment'),
+
+    path("contact",views.contact,name='contact'),
+    path('feedback', views.feedback_view, name='feedback'),
+    path('carbookcal', views.carbookcalculator, name='carbookcal'),
+
+]
